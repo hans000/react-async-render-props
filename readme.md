fb7cb633a0220c41da99356a3de7bba6887a7eec
hans000 <hhance@163.com>
2024-04-20 13:39:07 +0800

    feat: add useActionHandler

--- readme.md
+++ readme.md
@@ -5,16 +5,16 @@ It is a component to build children by resolving a promise with Render Props pat
 ## Usage
 
 ```
-import AsyncRender, { ActionRefType } from 'react-async-render-props'
+import AsyncRender, { type ActionType, useActionHandler } from 'react-async-render-props'
 
-export default function App() {
-    const actionRef = useRef<ActionRefType<SelectOption[]>>()
+function Foo() {
+    const actionRef = useActionHandler()
     return (
-        <div>
-            <div onClick={() => {
-                actionRef.current.run()
-            }}>clickme</div>
-            <AsyncRender actionRef={actionRef} request={() => {
+        <>
+            <Button onClick={() => {
+                actionRef.current!.runAsync()
+            }}>fetch</Button>
+            <AsyncRender id='fooId' actionRef={actionRef} request={() => {
                 return new Promise<SelectOption[]>(resolve => {
                     setTimeout(() => {
                         resolve([
@@ -27,15 +27,29 @@ export default function App() {
                 })
             }}>
                 {
-                    ({ loading, data, run }) => {
+                    ({ loading, data, runAsync }) => {
                         return (
                             <Spin spinning={loading}>
+                                <Button onClick={() => {
+                                    runAsync()
+                                }}>fetch</Button>
                                 <ProFormSelect options={data}/>
                             </Spin>
                         )
                     }
                 }
             </AsyncRender>
+        </>
+    )
+}
+
+export default function App() {
+    return (
+        <div>
+            <div onClick={() => {
+                AsyncRender.getHandler('fooId').runAsync()
+            }}>clickme</div>
+            <Foo />
         </div>
     )
 }
@@ -45,13 +59,16 @@ export default function App() {
 
 |Name|Type|Description|
 |--|--|--|
-|request|(...params: P) => Promise\<T\>|Required, A function returning a promise (it will be called when the component is mounted)|
-|children|(result: ReturnType\<typeof useRequest\<T, P\>\>) => React.ReactElement|Required, A render props function|
-|actionRef|React.MutableRefObject\<ActionRefType\<T, P\>\>|get useRequest result|
+|request|`(...params: P) => Promise\<T\>`|Required, A function returning a promise (it will be called when the component is mounted)|
+|children|`(result: ReturnType\<typeof useRequest\<T, P\>\>) => React.ReactElement`|Required, A render props function|
+|actionRef|`React.MutableRefObject\<ActionType\<T, P\>\>`|get useRequest result|
 |options|-|useRequest options|
+|id|`string`|mark instance|
+|forceUpdate|`boolean`|force update when fired|
+|AsyncRender.getHandler|`(id: string) => ActionType`|get action handler|
 
 ```
-type ActionRefType<T = any, P extends any[] = any> = ReturnType<typeof useRequest<T, P>> & {
+type ActionType<T = any, P extends any[] = any> = ReturnType<typeof useRequest<T, P>> & {
   setParam: (partialParam: any, index?: number) => void
 }
 ```
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import { useRequest } from "ahooks"
-import { Fragment, useRef } from "react"
+import { useLatest, useMemoizedFn, useRequest } from "ahooks"
+import React, { useEffect, useMemo } from "react"
+import { useRef } from "react"
 
-export type ActionRefType<T = any, P extends any[] = any> = ReturnType<typeof useRequest<T, P>> & {
+type GetPrefixed<T> = `get${Capitalize<string & T>}`
+
+type ToGetMethod<T> = {
+  [K in keyof T as T[K] extends (...args: any[]) => any ? K : GetPrefixed<K>]: T[K] extends (...args: any[]) => any ? T[K] : (() => T[K])
+}
+
+export type ActionType<T = any, P extends any[] = any[]> = ToGetMethod<ReturnType<typeof useRequest<T, P>>> & {
   setParam: (partialParam: any, index?: number) => void
 }
 
 const ModifiedMethods = ['run', 'runAsync', 'mutate', 'refresh', 'refreshAsync']
-
-function createSeed() {
-  return Math.random().toString(36).slice(2)
-}
 
 function merge(targetList: any[] = [], sourceList: any[] = []) {
   const a = targetList.map((item, index) => {
@@ -23,41 +26,100 @@ function merge(targetList: any[] = [], sourceList: any[] = []) {
   return [...a, ...b]
 }
 
-function modify(object: ReturnType<typeof useRequest>, seedRef: React.MutableRefObject<string>, params: any[]) {
+function modify(object: Record<string, any>, seedRef: React.MutableRefObject<string>, params: any[]) {
   ModifiedMethods.forEach(name => {
-    //@ts-ignore
     const originMethod = object[name]
-    //@ts-ignore
-    object[name] = (...args: any[]) => {
-      seedRef.current = createSeed()
-      return originMethod(...merge(args, params))
+    if (originMethod) {
+      object[name] = (...args: any[]) => {
+        seedRef.current = Math.random().toString(36)
+        return originMethod(...merge(args, params))
+      }
     }
   })
 }
 
+export function useActionHandler(): React.MutableRefObject<ActionType | undefined>
+export function useActionHandler<T extends readonly string[]>(nameList: T): Record<T[number], React.MutableRefObject<ActionType | undefined>>
+export function useActionHandler(nameList?: string[]): unknown {
+  const ref = useRef<unknown>({})
+  return nameList === undefined ? (ref.current = undefined, ref) : nameList.reduce((acc, name) => (acc[name] = {}, acc), ref.current as Record<string, unknown>)
+}
+
+const cacheMap = new Map<string, ToGetMethod<ActionType>>()
+
+function getHandler(id: string) {
+  const handle = cacheMap.get(id)
+  if (!handle) {
+    throw new ReferenceError(`can not found '${id}' id`)
+  }
+  return handle
+}
+
+function omit<T, Ks extends (keyof T)[]>(obj: T, ...keys: Ks) {
+  const result: Record<string, unknown> = {}
+  Object.keys(obj as object).forEach((k) => {
+    if (!keys.includes(k as keyof T)) {
+      result[k] = (obj as any)[k]
+    }
+  })
+  return result as T
+}
+
+AsyncRender.getHandler = getHandler
+
 export default function AsyncRender<T, P extends any[]>(props: {
-  actionRef?: React.MutableRefObject<ActionRefType<T, P>>
+  actionRef?: React.MutableRefObject<ActionType | undefined>
+  /** useRequest的service选项 */
   request: (...params: P) => Promise<T>
+  /** useRequest的options选项 */
   options?: Parameters<typeof useRequest<T, P>>[1]
   children: (result: ReturnType<typeof useRequest<T, P>>) => React.ReactElement
+  /** 强制更新，内部使用一个随机的key触发渲染 */
+  forceUpdate?: boolean
+  /** 设置了id后会缓存当前实例，需要全局唯一 */
+  id?: string
 }) {
   const result = useRequest(props.request, props.options)
   const seedRef = useRef('')
   const paramsRef = useRef<any[]>([])
+  const memorizedChildren = useMemoizedFn(props.children)
+  const resultRef = useLatest(result)
 
   modify(result, seedRef, paramsRef.current)
 
-  if (props.actionRef) {
-    props.actionRef.current = {
-      ...result,
-      setParam(partialParam, index = 0) {
-        if (!paramsRef.current[index]) {
-          paramsRef.current[index] = {}
-        }
-        Object.assign(paramsRef.current[index], partialParam)
-      },
-    }
+  const handle: ActionType = {
+    ...omit(resultRef.current, 'data', 'error', 'params'),
+    setParam(partialParam, index = 0) {
+      if (!paramsRef.current[index]) {
+        paramsRef.current[index] = {}
+      }
+      Object.assign(paramsRef.current[index], partialParam)
+    },
+    getLoading: () => resultRef.current.loading,
+    getParams: () => resultRef.current.params,
+    getData: () => resultRef.current.data,
+    getError: () => resultRef.current.error,
   }
 
-  return <Fragment key={seedRef.current}>{props.children(result)}</Fragment>
+  if (props.actionRef) {
+    props.actionRef.current = handle
+  }
+
+  const dom = useMemo(() => memorizedChildren(result), [result])
+
+  useEffect(() => {
+    if (props.id) {
+      if (cacheMap.has(props.id)) {
+        throw new Error(`Warning: ${props.id} has been used.`)
+      }
+      cacheMap.set(props.id, handle)
+      return () => {
+        cacheMap.delete(props.id!)
+      }
+    }
+  }, [props.id])
+
+  return (
+    <React.Fragment key={props.forceUpdate ? seedRef.current : undefined}>{dom}</React.Fragment>
+  )
 }
